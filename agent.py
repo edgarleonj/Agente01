c0c33011923b9dc0db01265d3165df4fdc2ff3cd
Edgar Leon Jaimes <edgar.leonj@gmail.com>
2026-02-14 22:52:30 -0600

    Inicio limpio: Agente con uv

---
+++ agent.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from groq import Groq
+import os
+
+
+load_dotenv()
+
+api_key = os.environ.get("GROQ_API_KEY")
+if not api_key:
+    print("❌ Error: No se encontró la GROQ_API_KEY en el archivo .env")
+    exit()
+
+client = Groq(api_key=api_key)
+
+
+print("Hello, World!")
+
+
+messages = [
+    {"role": "user", "content": "You are a helpful assistant."},
+]
+
+resp = client.chat.completions.create(model="qwen/qwen3-32b", messages=messages)
+
+msg = resp.choices[0].message.content
+assistant_text = msg.content or ""
+print(f"Asistente: {assistant_text}")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agente!")
+
+
+if __name__ == "__main__":
+    main()
