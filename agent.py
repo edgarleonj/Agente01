a1fce6577f9e701637205b35142173b38ebc5ccc
Edgar Leon Jaimes <edgar.leonj@gmail.com>
2026-02-15 21:48:48 -0600

    Actualización: Configuracion del Whilt

--- agent.py
+++ agent.py
@@ -15,17 +15,24 @@ client = Groq(api_key=api_key)
 
 print("Hello, World!")
 
+while True:
 
-messages = [
-    {"role": "user", "content": "You are a helpful assistant."},
-]
+    user_text = input("Tu: ").strip()
+    if not user_text:
+        continue
+    if user_text.lower() in ("exit","Salir"):
+        print("Adiós")
+        break
+    messages = [
+        {"role": "user", "content": user_text},
+    ]
 
-resp = client.chat.completions.create(model="qwen/qwen3-32b", messages=messages)
+    resp = client.chat.completions.create(model="qwen/qwen3-32b", messages=messages)
 
-#msg = resp.choices[0].message.content
-#assistant_text = msg.content or ""
-#print(f"Asistente: {assistant_text}")
+    #msg = resp.choices[0].message.content
+    #assistant_text = msg.content or ""
+    #print(f"Asistente: {assistant_text}")
 
-# La forma correcta para la versión actual de la librería:
-assistant_text = resp.choices[0].message.content
-print(assistant_text)
+    # La forma correcta para la versión actual de la librería:
+    assistant_text = resp.choices[0].message.content
+    print(assistant_text)
